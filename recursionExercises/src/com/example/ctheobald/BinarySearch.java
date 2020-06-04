e2e0890845aa19aeb33bf8fb67adf93303d53354
Christine Theobald <christinetheobald@icloud.com>
2020-06-04 13:34:13 -0400

    removed commented code

--- recursionExercises/src/com/example/ctheobald/BinarySearch.java
+++ recursionExercises/src/com/example/ctheobald/BinarySearch.java
@@ -30,34 +30,6 @@ public class BinarySearch {
                 return -1;
             }
             return binarySearch(value, Arrays.copyOfRange(sortedArray, lowerBound, upperBound));
-//        int midPoint = -1;
-//
-//        boolean found = false;
-//
-//        while(!found) {
-//            if (upperBound < lowerBound) {
-//                System.out.println(value + " does not exist.");
-//                return -1;
-//            }
-//
-//            midPoint = lowerBound + (upperBound - lowerBound)/2;
-//
-//            if (sortedArray[midPoint] < value) {
-//                lowerBound = midPoint + 1;
-//            }
-//
-//            if (sortedArray[midPoint] > value) {
-//                upperBound = midPoint - 1;
-//            }
-//
-//            if (sortedArray[midPoint] == value) {
-//                System.out.println(value + " found at location " + midPoint);
-//                found = true;
-//
-//            }
-//        }
-//
-//        return midPoint;
         }
     }
 }
